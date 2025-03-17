1478a751005350632bd6e791c91aa0c6a87f0c42
MMirza123 <Markusmirza030728@gmail.com>
2025-03-17 16:19:16 +0100

    Lite ändringar

--- MotionConnect/Controllers/InlaggController.cs
+++ MotionConnect/Controllers/InlaggController.cs
@@ -8,15 +8,19 @@ using Microsoft.AspNetCore.Http;
 public class InlaggController : Controller 
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public InlaggController(UserManager<ApplicationUser> userManager)
+    public InlaggController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     [HttpGet]
-    public IActionResult SkapaEttInlagg()
+    public async Task<IActionResult> SkapaEttInlagg()
     {
-        return View();
+        var vemInloggad = User.Identity.Name;
+        var anvandare = await _userManager.FindByEmailAsync(vemInloggad);
+        return View(anvandare);
     }
 }
