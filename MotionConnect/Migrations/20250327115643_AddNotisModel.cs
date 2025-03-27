d956e4c007bfa93ebf2ba37c5fa216722473a41b
MMirza123 <Markusmirza030728@gmail.com>
2025-03-27 13:08:28 +0100

    Tabell uppdatering

---
+++ MotionConnect/Migrations/20250327115643_AddNotisModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotionConnect.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotisModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Meddealande",
+                table: "Notiser",
+                newName: "Meddelande");
+
+            migrationBuilder.AddColumn<int>(
+                name: "InlaggId",
+                table: "Notiser",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MeddelandeId",
+                table: "Notiser",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Typ",
+                table: "Notiser",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notiser_InlaggId",
+                table: "Notiser",
+                column: "InlaggId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notiser_Inlagg_InlaggId",
+                table: "Notiser",
+                column: "InlaggId",
+                principalTable: "Inlagg",
+                principalColumn: "InlaggId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notiser_Inlagg_InlaggId",
+                table: "Notiser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notiser_InlaggId",
+                table: "Notiser");
+
+            migrationBuilder.DropColumn(
+                name: "InlaggId",
+                table: "Notiser");
+
+            migrationBuilder.DropColumn(
+                name: "MeddelandeId",
+                table: "Notiser");
+
+            migrationBuilder.DropColumn(
+                name: "Typ",
+                table: "Notiser");
+
+            migrationBuilder.RenameColumn(
+                name: "Meddelande",
+                table: "Notiser",
+                newName: "Meddealande");
+        }
+    }
+}
--- MotionConnect/Models/Notis.cs
+++ MotionConnect/Models/Notis.cs
@@ -1,15 +1,32 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 public class Notis
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int NotisId { get; set; }
-    public string Meddealande { get; set; }
-    public bool ArLast { get; set; }
-    public DateTime SkapadesTid { get; set; }
 
+    [Required]
+    public string Meddelande { get; set; }
+
+    public bool ArLast { get; set; } = false;
+
+    public DateTime SkapadesTid { get; set; } = DateTime.UtcNow;
+
+    [Required]
     public string AnvandarId { get; set; }
     public ApplicationUser Anvandare { get; set; }
+
+    [Required]
+    public NotisTyp Typ { get; set; }
+
+    // Valfria kopplingar
+    public int? InlaggId { get; set; }
+    public Inlagg Inlagg { get; set; }
+
+    public int? MeddelandeId { get; set; }
+    // Om du har en Meddelande-klass, annars kan du lägga till senare
 }
+
